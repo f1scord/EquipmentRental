871595dbb83924bf1f719ddb3f51e13099563e5e
Anton Ovcharenko <toha.ovcharenko@gmail.com>
2026-03-17 15:24:25 +0100

    Add a Laptop class with two fields I guess are the most inportant specs for the laptop

---
+++ EquipmentRental/Models/Laptop.cs
@@ -0,0 +1,18 @@
+﻿namespace EquipmentRental.Models;
+
+public class Laptop : Equipment
+{
+    public int SizeInches {get; set;}
+    public int RamGb {get; set;}
+
+    public Laptop(string name, int sizeInches, int ramGg) : base(name)
+    {
+        SizeInches = sizeInches;
+        RamGb = ramGg;
+    }
+
+    public override string GetInfo()
+    {
+        return $"Laptop Size: {SizeInches}, RAM: {RamGb}GB";
+    }
+}
