d2257bb40f8383ee41c29d91220dc84118e1b81e
Anton Ovcharenko <toha.ovcharenko@gmail.com>
2026-03-17 15:13:55 +0100

    Add Equipment abstract class with one abstract method for other subclasses to implement

---
+++ EquipmentRental/Models/Equipment.cs
@@ -0,0 +1,17 @@
+﻿namespace EquipmentRental.Models;
+
+public abstract class Equipment
+{
+    public Guid Id { get; private set; }
+    public string Name { get; set; }
+    public bool IsAvailable  { get; set; }
+
+    protected Equipment(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        IsAvailable = true;
+    }
+    
+    public abstract string GetInfo();
+}
