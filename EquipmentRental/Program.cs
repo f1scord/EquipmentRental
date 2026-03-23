e246baf801f3c4873a4bc411ad877893ab8b67fb
Anton Ovcharenko <123882650+f1scord@users.noreply.github.com>
2026-03-23 17:30:01 +0100

    Add demo scenario in Program.cs

--- EquipmentRental/Program.cs
+++ EquipmentRental/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EquipmentRental.Models;
+using EquipmentRental.Services;
+var rentalService = new RentalService();
+var reportService = new ReportService(rentalService);
+var laptop = new Laptop("Dell XPS", 15, 16);
+var projector = new Projector("Epson EB", 3500, true);
+var camera = new Camera("Canon EOS", 24, "Canon");
+rentalService.AddEquipment(laptop);
+rentalService.AddEquipment(projector);
+rentalService.AddEquipment(camera);
+var student = new Student("Anton", "Ovcharenko");
+var employee = new Employee("Vadim", "Ovcharenko");
+rentalService.AddUser(student);
+rentalService.AddUser(employee);
+Console.WriteLine(rentalService.RentEquipment(student.Id, laptop.Id));
+Console.WriteLine(rentalService.RentEquipment(employee.Id, laptop.Id));
+Console.WriteLine(rentalService.RentEquipment(student.Id, projector.Id));
+Console.WriteLine(rentalService.RentEquipment(student.Id, camera.Id));
+var activeRentals = rentalService.GetActiveRentals(student.Id);
+Console.WriteLine(rentalService.ReturnEquipment(activeRentals[0].Id));
+reportService.PrintReport();
